d4c2d775297483dc4ea7e864f022265b9e844745
GlebMizin <112752101+GlebMizin@users.noreply.github.com>
2022-10-29 12:39:07 +0300

    Update FirstTask.py

--- PyCharm/Individual tasks/FirstTask.py
+++ PyCharm/Individual tasks/FirstTask.py
@@ -17,17 +17,17 @@ if __name__ == "__main__":
     ]
 
     # Number input
-    C_number = int(input("Enter number (|C|<9): "))
+    number = int(input("Enter number (|C|<9): "))
 
     # Number range check
-    if C_number <= -9 or C_number >= 9:
+    if number <= -9 or number >= 9:
         print("Your number is out of range")
 
     # Displaying a number with a sign
     else:
-        if C_number < 0:
-            print('Your number is: minus ' + NumList[(C_number*-1)-1])
-        elif C_number > 0:
-            print('Your number is: ' + NumList[C_number-1])
+        if number < 0:
+            print('Your number is: minus ' + NumList[(number*-1)-1])
+        elif number > 0:
+            print('Your number is: ' + NumList[number-1])
         else:
             print('Your number is: zero')
