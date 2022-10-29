0b12c407c7d9cb1b6155015e00850a7aaa4c6dbc
GlebMizin <112752101+GlebMizin@users.noreply.github.com>
2022-10-29 12:39:58 +0300

    Update SecondTask.py

--- PyCharm/Individual tasks/SecondTask.py
+++ PyCharm/Individual tasks/SecondTask.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     b1, b2 = map(int, input("Enter second point coordinates: ").split())
 
     # Calculating the distance to a point from the origin
-    A_point = math.sqrt(a1 ** 2 + a2 ** 2)
-    B_point = math.sqrt(b1 ** 2 + b2 ** 2)
+    point1 = math.sqrt(a1 ** 2 + a2 ** 2)
+    point2 = math.sqrt(b1 ** 2 + b2 ** 2)
 
     # Comparison of the obtained values and conclusion
-    if A_point > B_point:
+    if point1 > point2:
         print("Point A is farther than B")
-    elif A_point < B_point:
+    elif point1 < point2:
         print("Point B is farther than A")
     else:
         print("Points are on the same distance")
