5fcbcce3e8d1ec3edfa12c50e85a159d22f0660f
GlebMizin <112752101+GlebMizin@users.noreply.github.com>
2022-11-05 10:31:16 +0300

    Update AdvancedTask.py

--- PyCharm/Individual tasks/AdvancedTask.py
+++ PyCharm/Individual tasks/AdvancedTask.py
@@ -13,14 +13,15 @@ if __name__ == "__main__":
         print("Illegal value of X", file=sys.stderr)
         exit(1)
 
-    a = x
+    a = -3 * x ** 2 / 100
     Sum, n = a, 1
 
     # Find the sum of the terms of the series.
     while math.fabs(a) > EPS:
-        a *= -1 * (2 * n + 3) ** 2 * (2 * n + 2) / x ** 2 * (2 * n + 1)
+        a *= (-x ** 2 * (2 * n + 1)) / ((2 * n + 3) ** 2 * (2 * n + 2))
         Sum += a
         n += 1
 
     # Function value output.
     print(Sum)
+    
